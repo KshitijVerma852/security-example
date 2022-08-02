99c3829902584797d56c9ffb967c5a379cf2ca2b
kshitijverma197 <kshitijverma197@gmail.com>
2022-08-02 14:39:42 +0100

    security example till 2nd Aug

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
+const fs = require("fs");
 const path = require("path");
+const https = require("https");
 const express = require("express");
-const PORT = 3000;
-const app = express()
+const helmet = require("helmet");
 
+const PORT = 7000;
+
+const app = express();
+app.use(helmet());
+
+app.get("/secret", (req, res) => {
+  return res.send("ehh");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("./key.pem"),
+      cert: fs.readFileSync("./cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
