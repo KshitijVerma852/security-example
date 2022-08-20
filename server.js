eb59b4a7bca82381079be077d4dbcc527a10f2eb
Kshitij <thisisdinku@outlook.com>
2022-08-20 14:11:43 +0100

    Auth skeleton + passport packages.

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ const PORT = 7000;
 const app = express();
 app.use(helmet());
 
-app.get("/secret", (req, res) => {
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true; // TODO
+    if (!isLoggedIn) {
+        return res.status(401).json({
+            error: "You must log in."
+        });
+    }
+    next()
+}
+
+app.get("/auth/google", (req, res) => {})
+
+app.get("/auth/google/callback", (req, res) => {})
+
+app.get("/auth/logout", (req, res) => {})
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("ehh");
 });
 
@@ -26,5 +42,5 @@ https
     app
   )
   .listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
+    console.log(`https://localhost:${PORT}`);
   });
